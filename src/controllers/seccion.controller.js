ebf7e75da0eb7fdcfde407126120e006259be20c
cabal <cabal@ATHENA>
2025-10-06 17:41:39 -0600

    Se agrego cache a get Secciones

--- src/controllers/seccion.controller.js
+++ src/controllers/seccion.controller.js
@@ -1,25 +1,42 @@
 import { pool } from "../DB.js";
+import NodeCache from 'node-cache';
+
+//cache en memoria
+const cache = new NodeCache({stdTTL: 10800, checkperiod: 1800});
 
 //Mostrar todas las secciones con su informacion y datos de imagen
 export const getSection = async (req, res) => {
   try {
+    //Si existen datos en cache los devuelve
+    const cacheData = cache.get('secciones')
+
+    if (cacheData) {
+      console.log("Datos desde cache")
+      return res.json(cacheData);
+    }
+
     //Sentencia SQL
     const query =
       "SELECT nombre_seccion, informacion, imagenURL \n" +
-      "FROM secciones s JOIN railway.informacion i ON s.id_seccion = i.id_informacion \n" +
+      "FROM secciones s JOIN colegio.informacion i ON s.id_seccion = i.id_informacion \n" +
       "JOIN imagenes img ON s.id_seccion = img.id_imagen";
 
-      //Trae los datos
+    //Trae los datos
     const [rows] = await pool.query(query);
+
+    //Almacenar en cache
+    cache.set('secciones', rows);
 
     //Muestra los datos encontrados
     res.json(rows);
 
   } catch (error) {
-    console.log("Problema : " + error)
+
+    console.log("Problema : " + error);
+
     return res.status(500).json({
       message: "Algo salio mal" + error,
     });
+
   }
 };
-
