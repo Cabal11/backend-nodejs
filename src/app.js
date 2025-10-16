774bc3339be4e26ed4358494e7f0b3ae88f3d7fb
cabal <cabal@ATHENA>
2025-10-16 14:13:57 -0600

    Se agrego cors para multiples solicitudes de diferente dominio

--- src/app.js
+++ src/app.js
@@ -1,16 +1,36 @@
 import express from "express";
+import cors from 'cors'
 import seccionRoutes from "./routes/seccion.routes.js";
 import requisitosRoute from "./routes/requisitos.routes.js";
 import cronogramaRoute from "./routes/cronograma.routes.js";
 
+
 const app = express();
+
 
 //Recibir los datos y convertilos en json
 app.use(express.json());
+app.use(cors({
+  origin: 'https://liceonosaradev.netlify.app',
+  credentials: true,
+  methods: 'GET'
+}))
+// app.use(cors({
+//origin: 'url',
+//credentials: true,
+//methods: GET
+// }))
 
 app.use("/api", seccionRoutes);
 app.use("/api", requisitosRoute);
 app.use("/api", cronogramaRoute);
+app.use("/ping", async (req, res) => {
+  try {
+    res.status(200).json("ping");
+  } catch (error) {
+    res.status(500).json("Servidor en reposo");
+  }
+});
 
 // Si ingresan la ruta incorrecta o que no existe
 app.use((req, res, next) => {
