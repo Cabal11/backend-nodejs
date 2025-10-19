67158568dc6d3bd2639657cabf6d3e8b8f996821
cabal <cabal@ATHENA>
2025-10-18 21:58:38 -0600

    (add) se agrego mas seguridad

--- src/controllers/Auth/verificarToken.js
+++ src/controllers/Auth/verificarToken.js
@@ -1,14 +1,34 @@
+import jwt from "jsonwebtoken";
+import { NAME_KEY, JWT_SECRET } from "../../config.js";
+
 function verificarToken(req, res, next) {
-  const token = req.headers["x-access-token"];
+  try {
+    const token = req.headers["x-access-token"];
 
-  if (!token) {
-    return res.status(401).json({
-      auth: false,
-      message: "No autorizado",
-    });
+    //validar si tiene el token o no
+    if (!token) {
+      return res.status(401).json({
+        auth: false,
+        message: "No autorizado",
+      });
+    }
+
+    //Decodificar para validar
+    const decode = jwt.verify(token, JWT_SECRET);
+
+    //Comprobar si es el mismo
+    if (decode.name != NAME_KEY) {
+      return res.status(401).json({
+        auth: false,
+        message: "No permitido",
+      });
+    }
+
+    next();
+  } catch (error) {
+    //Por si esta alterado el token
+    return res.status(500).json("Token invalido");
   }
-
-  next();
 }
 
 export default verificarToken;
